8e6927fa1a770155d3c83657a14f8321d93de876
Henrikas Butenas <butenashenrikas@gmail.com>
2022-03-09 13:29:16 +0200

    user authentication functionallity added

--- server/middlewares/auth/authMiddleware.js
+++ server/middlewares/auth/authMiddleware.js
@@ -1,6 +1,6 @@
 const validator = require('validator');
 const CustomError = require('../../errors');
-
+const { verifyToken } = require('../../utils/jwtUtils');
 const registerMiddleware = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -51,10 +51,53 @@ const loginMiddleware = async (req, res, next) => {
 
 const verifyMiddleware = async (req, res, next) => {
   const { refresh_token, access_token } = req.cookies;
+
   if (!refresh_token && !access_token) {
     throw new CustomError.Unauthenticated('Authentication invalid');
   }
 
   next();
 };
-module.exports = { registerMiddleware, loginMiddleware, verifyMiddleware };
+
+const authenticateUser = async (req, res, next) => {
+  const { refresh_token, access_token } = req.cookies;
+
+  if (!refresh_token && !access_token) {
+    throw new CustomError.Unauthenticated('Authentication invalid');
+  }
+
+  if (access_token) {
+    const user = await verifyToken(access_token, process.env.ACCESS_TOKEN);
+    if (!user) {
+      throw new CustomError.Unauthenticated('Authentication invalid');
+    } else {
+      req.user = user;
+      next();
+    }
+  } else if (refresh_token) {
+    const user = await verifyToken(refresh_token, process.env.REFRESH_TOKEN);
+    if (!user) {
+      throw new CustomError.Unauthenticated('Authentication invalid');
+    } else {
+      req.user = user;
+      next();
+    }
+  }
+};
+
+const authenticatePermissions = (...roles) => {
+  return async (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.Unauthorized('Unauthorized to access this route');
+    }
+    next();
+  };
+};
+
+module.exports = {
+  registerMiddleware,
+  loginMiddleware,
+  verifyMiddleware,
+  authenticateUser,
+  authenticatePermissions
+};
