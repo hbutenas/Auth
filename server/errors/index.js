077a6f2a8a3d3640fd48b19e0b1ea095afd2b4c8
Henrikas Butenas <butenashenrikas@gmail.com>
2022-03-09 13:31:41 +0200

    error export

--- server/errors/index.js
+++ server/errors/index.js
@@ -2,12 +2,12 @@ const CustomError = require('./types/CustomError');
 const BadRequest = require('./types/BadRequest');
 const InternalServer = require('./types/InternalServer');
 const Unauthenticated = require('./types/Unauthenticated');
-// const Unauthorized = require("./types/Unauthorized");
+const Unauthorized = require('./types/Unauthorized');
 
 module.exports = {
   CustomError,
   BadRequest,
   InternalServer,
-  Unauthenticated
-  // Unauthorized
+  Unauthenticated,
+  Unauthorized
 };
